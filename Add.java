1c61ed4ffe3a1d2a8f25f616b59e6b3b9f8ad767
sumita12345 <sumitamsomankatti@gmail.com>
2025-04-09 08:26:57 +0530

    save

---
+++ Add.java
@@ -0,0 +1,11 @@
+class Add{
+    public static void main(String[] args) {
+        int a,b;
+        a=10;
+        b=5;
+        int sum=a+b;
+        System.out.println("Addition of a & b is:"+sum);
+
+    }
+}
+        
